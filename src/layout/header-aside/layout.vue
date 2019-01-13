d47485e293a97c54417f2c0f86e506ae2d051dd7
kaiyouhu <kaiyouhu@gmail.com>
2019-01-13 17:55:14 +0800

    修改icon

--- src/layout/header-aside/layout.vue
+++ src/layout/header-aside/layout.vue
@@ -15,9 +15,9 @@
         }"
         flex-box="0"
         flex>
-        <div class="logo-group" :style="{width: asideCollapse ? asideWidthCollapse : asideWidth}" flex-box="0">
-          <img v-if="asideCollapse" :src="`${$baseUrl}image/theme/${themeActiveSetting.name}/logo/icon-only.png`">
-          <img v-else :src="`${$baseUrl}image/theme/${themeActiveSetting.name}/logo/all.png`">
+        <div class="c logo-group" :style="{width: asideCollapse ? asideWidthCollapse : asideWidth}" flex-box="0">
+          <img v-if="asideCollapse" :src="`${$baseUrl}image/theme/${themeActiveSetting.name}/logo/TMESIS-icon-only.png`">
+          <img v-else :src="`${$baseUrl}image/theme/${themeActiveSetting.name}/logo/TMESIS-all.png`">
         </div>
         <div class="toggle-aside-btn" @click="handleToggleAside" flex-box="0">
           <d2-icon name="bars"/>
--- src/setting.js
+++ src/setting.js
@@ -45,24 +45,24 @@ const setting = {
       {
         title: '紫罗兰',
         name: 'violet',
-        preview: 'image/theme/violet/preview@2x.png'
+        preview: 'image/theme/violet/TMESIS-preview@2x.png'
       },
       {
         title: '简约线条',
         name: 'line',
         backgroundImage: 'image/theme/line/bg.jpg',
-        preview: 'image/theme/line/preview@2x.png'
+        preview: 'image/theme/line/TMESIS-preview@2x.png'
       },
       {
         title: '流星',
         name: 'star',
         backgroundImage: 'image/theme/star/bg.jpg',
-        preview: 'image/theme/star/preview@2x.png'
+        preview: 'image/theme/star/TMESIS-preview@2x.png'
       },
       {
         title: 'Tomorrow Night Blue (vsCode)',
         name: 'tomorrow-night-blue',
-        preview: 'image/theme/tomorrow-night-blue/preview@2x.png'
+        preview: 'image/theme/tomorrow-night-blue/TMESIS-preview@2x.png'
       }
     ]
   },
